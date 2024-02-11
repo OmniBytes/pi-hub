10daabc174d8dd3f824dcea3d9a5e1a0c0bddd51
funkstyr <30911142+funkstyr@users.noreply.github.com>
2024-02-11 14:08:53 -0500

    feat: add weather route

---
+++ packages/api/src/router/weather.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+import { handleApiResponse } from "../handle-api-response";
+// import { CreatePostSchema } from "@omnibytes/validators";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const postRouter = createTRPCRouter({
+  getInfo: publicProcedure
+    .input(z.object({ lat: z.string(), long: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const { weatherApi } = ctx;
+      const point = `${input.lat},${input.long}`;
+
+      const resp = await weatherApi.GET("/points/{point}", {
+        params: {
+          path: { point },
+        },
+      });
+
+      return handleApiResponse(resp);
+    }),
+});
