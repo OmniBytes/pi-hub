f5ee2ab04981d1d67f8dc799dffb827ebb3db620
funkstyr <funkstyr@gmail.com>
2024-02-12 00:34:28 -0500

    feat: call weather api

--- packages/api/src/router/root.ts
+++ packages/api/src/router/root.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from "../trpc";
 import { authRouter } from "./auth";
 import { postRouter } from "./post";
+import { weatherRouter } from "./weather";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   post: postRouter,
+  weather: weatherRouter,
 });
 
 // export type definition of API
--- packages/weather/src/weather-card.tsx
+++ packages/weather/src/weather-card.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 import { useGeolocation } from "@uidotdev/usehooks";
+
+import { api } from "@omnibytes/trpc/react";
 
 export function WeatherCard() {
   const location = useGeolocation();
+  const haveLocation = !!location.latitude && !!location.longitude;
 
-  useEffect(() => {
-    if (location.latitude && location.longitude) {
-      console.log("location", location);
-    }
-  }, [location.latitude, location.longitude]);
+  const weather = api.weather.getInfo.useQuery(
+    { lat: location.latitude, long: location.longitude },
+    {
+      enabled: !haveLocation,
+    },
+  );
+
+  console.log("get info", weather);
 
   return <Fragment>weather</Fragment>;
 }
