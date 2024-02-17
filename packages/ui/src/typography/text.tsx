2f86c563b0191895cfc27e2bd93f9443c5a9784b
Michael Funk <30911142+funkstyr@users.noreply.github.com>
2024-02-16 21:44:50 -0500

    feat: create text and title components (#49)
    * feat: add text component
    
    * feat: add title
    
    * feat: add block quote and code component
    
    * wip: export new components

---
+++ packages/ui/src/typography/text.tsx
@@ -0,0 +1,39 @@
+import type { VariantProps } from "class-variance-authority";
+import * as React from "react";
+import { cva } from "class-variance-authority";
+
+import { cn } from "..";
+
+const textVariants = cva("", {
+  variants: {
+    variant: {
+      default: "leading-7 [&:not(:first-child)]:mt-6",
+      lead: "text-xl text-muted-foreground",
+      large: "text-lg font-semibold",
+      small: "text-sm font-medium leading-none",
+      muted: "text-sm text-muted-foreground",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
+
+export interface TextProps extends VariantProps<typeof textVariants> {
+  children: string;
+  className?: string;
+}
+
+function Text(props: TextProps) {
+  const { children, className, variant, ...rest } = props;
+
+  return (
+    <p {...rest} className={cn(textVariants({ variant, className }))}>
+      {children}
+    </p>
+  );
+}
+
+Text.displayName = "Text";
+
+export { Text, textVariants };
