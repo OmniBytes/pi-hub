ec797812bda144ecb0c717da6a6cbbf87b23c047
Michael Funk <30911142+funkstyr@users.noreply.github.com>
2024-02-23 01:46:22 -0500

    fix: replace md table with components (#68)
    * fix: replace md table with components
    
    * feat: display post preview image

--- apps/www/src/app/blog/_components/mdx-content.tsx
+++ apps/www/src/app/blog/_components/mdx-content.tsx
@@ -1,7 +1,18 @@
 "use client";
 
 import type { MDXRemoteSerializeResult } from "next-mdx-remote";
+import Image from "next/image";
 import { MDXRemote } from "next-mdx-remote";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@omnibytes/ui/table";
+import { Title } from "@omnibytes/ui/title";
 
 interface MdxContentProps {
   content: MDXRemoteSerializeResult<
@@ -13,10 +24,28 @@ interface MdxContentProps {
 export function MdxContent(props: MdxContentProps) {
   const { content } = props;
 
-  const components = {};
+  const components = {
+    Image,
+
+    Table,
+    TableHeader,
+    TableHead,
+    TableRow,
+    TableBody,
+    TableCell,
+  };
 
   return (
     <article className="prose py-6 dark:prose-invert">
+      <Title>{content.frontmatter.title as string}</Title>
+
+      <Image
+        src={content.frontmatter.previewImage as string}
+        alt="blog preview image"
+        width="718"
+        height="404"
+      />
+
       <MDXRemote {...content} components={components} />
     </article>
   );
