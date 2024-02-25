af6991a7a7b88f92cecf86a0e19856b0413b8d54
funkstyr <funkstyr@gmail.com>
2024-02-25 11:00:51 -0500

    fix: add css vars for colors

--- tooling/tailwind/src/add-color-vars.ts
+++ tooling/tailwind/src/add-color-vars.ts
@@ -1,14 +1,16 @@
 // @ts-expect-error no types
 import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
+import { colors } from "./colors";
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function addColorVar({ addBase, theme }: any) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-  const colors = flattenColorPalette(theme.colors);
+export function addColorVar({ addBase }: any) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const _colors = flattenColorPalette(colors);
 
   const vars = Object.fromEntries(
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    Object.entries(colors).map(([color, value]) => [`--${color}`, value]),
+    Object.entries(_colors).map(([color, value]) => [`--${color}`, value]),
   );
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
