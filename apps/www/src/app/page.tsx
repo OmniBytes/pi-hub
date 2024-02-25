722ceec3aa30d42ac9904bcf5da3c5f4ab8ab759
funkstyr <funkstyr@gmail.com>
2024-02-25 14:00:56 -0500

    wip: try moving border cta

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,11 +1,7 @@
 import Link from "next/link";
 
-import { cn } from "@omnibytes/ui";
-import { buttonVariants } from "@omnibytes/ui/button";
-import {
-  TypewriterEffect,
-  TypewriterEffectSmooth,
-} from "@omnibytes/ui/typewritter-effect";
+import { MovingBorderButton } from "@omnibytes/ui/moving-border";
+import { TypewriterEffectSmooth } from "@omnibytes/ui/typewritter-effect";
 
 const GITHUB_URL = "https://github.com/omnibytes/pi-hub";
 const TWITTER_URL = "https://twitter.com/funkstyr";
@@ -16,11 +12,11 @@ export default async function RootPage() {
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <Link
-            href={TWITTER_URL}
+            href={GITHUB_URL}
             className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
             target="_blank"
           >
-            Follow along on Twitter
+            GitHub
           </Link>
 
           <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
@@ -46,13 +42,15 @@ export default async function RootPage() {
           />
 
           <div className="space-x-4">
-            <Link
-              href={GITHUB_URL}
-              target="_blank"
-              rel="noreferrer"
-              className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
-            >
-              GitHub
+            <Link href={TWITTER_URL} className="" target="_blank">
+              <div>
+                <MovingBorderButton
+                  borderRadius="1.75rem"
+                  className="border-neutral-200 bg-white text-black dark:border-slate-800 dark:bg-slate-900 dark:text-white"
+                >
+                  Follow along on Twitter
+                </MovingBorderButton>
+              </div>
             </Link>
           </div>
         </div>
